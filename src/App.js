8d5f44a2bbe9c1b30b896b7d34864aaac77352e2
mr_pranay <pranaythangellapally1029@gmail.com>
2024-08-25 22:35:02 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to portfolio</h1>
+      <h2>this is pranay thangellapalli</h2>
     </div>
   );
 }
